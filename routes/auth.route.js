e32032911321c8b628ee33501d76db9062b3fed6
jsqmicg <80492686+jsqmicg@users.noreply.github.com>
2021-08-19 13:01:57 -0500

    social_login (#1)

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,6 +1,31 @@
 const express = require("express");
 const router = express.Router();
+//validations
+const {
+  validRegister,
+  validLogin,
+  forgotPasswordValidator,
+  resetPasswordValidator,
+} = require("../helpers/valid");
 
-const { registerController } = require("../controllers/auth.controller.js");
-router.post("/register", registerController);
+//Load controllers
+const {
+  registerController,
+  activationController,
+  forgetController,
+  loginController,
+  resetController,
+  googleController,
+  facebookController,
+} = require("../controllers/auth.controller.js");
+router.post("/register", validRegister, registerController);
+router.post("/login", validLogin, loginController);
+router.post("/activation", activationController);
+router.put("/password/forget", forgotPasswordValidator, forgetController);
+router.put("/resetpassword", resetPasswordValidator, resetController);
+
+//social login
+router.post("/googlelogin", googleController);
+router.post("/facebooklogin", facebookController);
+
 module.exports = router;
