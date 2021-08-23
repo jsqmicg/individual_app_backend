f0aef999b7e981a2e653d27e895c1c017ab9eb36
jsqmicg <jsquinonezm@correo.udistrital.edu.co>
2021-08-23 14:43:21 -0500

    se añaden modelo y controlador de los pines

---
+++ controllers/pin.controller.js
@@ -0,0 +1,20 @@
+const Pin = require("../models/Pin.js");
+
+exports.createPinController = async (req, res) => {
+  const newPin = new Pin(req.body);
+  try {
+    const savedPin = await newPin.save();
+    res.status(200).json(savedPin);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+exports.getPinsController = async (req, res) => {
+  try {
+    const pins = await Pin.find();
+    res.status(200).json(pins);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+
 //validations
 const {
   validRegister,
@@ -18,11 +19,21 @@ const {
   googleController,
   facebookController,
 } = require("../controllers/auth.controller.js");
+
+//Pin controller
+const {
+  createPinController,
+  getPinsController,
+} = require("../controllers/pin.controller");
+
+//routes
 router.post("/register", validRegister, registerController);
 router.post("/login", validLogin, loginController);
 router.post("/activation", activationController);
 router.put("/password/forget", forgotPasswordValidator, forgetController);
 router.put("/resetpassword", resetPasswordValidator, resetController);
+router.post("/createpin", createPinController);
+router.get("/getpins", getPinsController);
 
 //social login
 router.post("/googlelogin", googleController);
